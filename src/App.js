443155d0e869933f8bf696172f566933cb2b4c14
Modron42 <mikhailwgs@gmail.com>
2024-04-23 22:06:30 +0200

    Added menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { GlobeEuropeAfrica, ThermometerHalf, Eye, PlugFill, Buildings, CarFrontFill, AirplaneFill, Flower1 } from 'react-bootstrap-icons';
 
 function App() {
     const [emissions, setEmissions] = useState([]);
@@ -37,7 +38,25 @@ function App() {
                     </Nav>
                 </Container>
             </Navbar>
-            <div>
+            <Navbar expand="lg" className="position-fixed">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav defaultActiveKey="/home" className="flex-column">
+                            <Navbar.Brand href="#home">Menu</Navbar.Brand>
+                            <Nav.Link href="/home"><GlobeEuropeAfrica /> Region Selection</Nav.Link>
+                            <Nav.Link href="/home"><Eye /> Company Profiles</Nav.Link>
+                            <Nav.Link href="/home"><ThermometerHalf /> Heating</Nav.Link>
+                            <Nav.Link href="/home"><PlugFill /> Electricity</Nav.Link>
+                            <Nav.Link href="/home"><Buildings /> Industry</Nav.Link>
+                            <Nav.Link href="/home"><CarFrontFill /> Cars and Vehicles</Nav.Link>
+                            <Nav.Link href="/home"><AirplaneFill /> Aviation</Nav.Link>
+                            <Nav.Link href="/home"><Flower1 /> Agriculture</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className="content">
                 <Container>
                     <h5>Countries</h5>
                     <Table responsive striped bordered hover size="sm">
