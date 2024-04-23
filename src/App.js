7d79ed484ee7ab487da6c20e47a90b0e710721f7
Modron42 <mikhailwgs@gmail.com>
2024-04-23 17:05:16 +0200

    Added header and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
     const [emissions, setEmissions] = useState([]);
@@ -13,19 +15,46 @@ function App() {
             .then((data) => {
                 setEmissions(data);
             });
-    }, []);    
+    }, []);
     return (
-        <Container>
-            <h5>Countries</h5>
-            <Table responsive striped bordered hover size="sm">
-                {emissions.map(element => 
-                <tr>
-                    <td>{element.Country}</td>
-                    <td>{element['2022']} Mt CO&#8322;</td>
-                </tr>
-                )}
-            </Table>
-        </Container>
+        <div>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src="earth.png"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="Earth logo"
+                        />
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">HOME</Nav.Link>
+                        <Nav.Link href="#data">DATA</Nav.Link>
+                        <Nav.Link href="#reports">REPORTS</Nav.Link>
+                        <Nav.Link href="#contact">CONTACT</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <div>
+                <Container>
+                    <h5>Countries</h5>
+                    <Table responsive striped bordered hover size="sm">
+                        {emissions.map(element =>
+                            <tr>
+                                <td>{element.Country}</td>
+                                <td>{element['2022']} Mt CO&#8322;</td>
+                            </tr>
+                        )}
+                    </Table>
+                </Container>
+                <footer>
+                    <h4>FOOTER</h4>
+                    <p>Powered by <a href="https://react-bootstrap.netlify.app/" target="_blank">React Bootstrap</a></p>
+                </footer>
+            </div>
+        </div>
     );
 }
 
